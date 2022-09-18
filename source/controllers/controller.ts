f7a0822948c1dd7e2c9ac7dd2cc921ace0e5d6b7
alienpdev <alienpdev@192.168.1.17>
2022-09-19 03:15:55 +0700

    List NFT

--- source/controllers/controller.ts
+++ source/controllers/controller.ts
@@ -1,7 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
+import { getParsedNftAccountsByOwner, isValidSolanaAddress, createConnectionConfig, resolveToWalletAddress, } from "@nfteyez/sol-rayz";
+import * as web3 from "@solana/web3.js";
 
 const getListNFT = async (req: Request, res: Response, next: NextFunction) => {
     // get some posts
+    let address = req.body.address;
+
+    const publicAddress = await resolveToWalletAddress({
+        text: address
+      });
+
+      
+    // let connect = new web3.Connection(web3.clusterApiUrl("devnet"));
+    // const connect = createConnectionConfig('devnet');
+    const nftArray = await getParsedNftAccountsByOwner({
+        publicAddress: publicAddress,
+        connection: new web3.Connection(web3.clusterApiUrl("devnet"))
+      });
 
     let arr = []
     for (let index = 0; index < 5; index++) {
@@ -9,7 +24,7 @@ const getListNFT = async (req: Request, res: Response, next: NextFunction) => {
         
     }
     return res.status(200).json({
-        message: arr
+        message: nftArray
     });
 };
 
